4517e5194c6881330b23cebd146f5e32dce63885
Triet Lai <laitr@stgeorge.com.au>
2021-08-10 11:40:13 +1000

    Added additional settings to nvim airline.

--- nvim/lua/plugins/gitgutter.lua
+++ nvim/lua/plugins/gitgutter.lua
@@ -1,13 +1,13 @@
 --vim.g.gitgutter_use_location_list = 1
 vim.g.gitgutter_max_signs = -1
 
-vim.cmd[[
-function! GitStatus()
-  let [a,m,r] = GitGutterGetHunkSummary()
-  return printf('+%d ~%d -%d', a, m, r)
-endfunction
-set statusline+=%{GitStatus()}
+-- vim.cmd[[
+-- function! GitStatus()
+--   let [a,m,r] = GitGutterGetHunkSummary()
+--   return printf('+%d ~%d -%d', a, m, r)
+-- endfunction
+-- set statusline+=%{GitStatus()}
 
-command! Gqf GitGutterQuickFix | copen
-]]
+-- command! Gqf GitGutterQuickFix | copen
+-- ]]
 
--- nvim/lua/plugins/vim-airline.lua
+++ nvim/lua/plugins/vim-airline.lua
@@ -1,4 +1,15 @@
 -- Disable displaying buffer-line as using akinsho/nvim-bufferline.lua plugin instead
 vim.g['airline#extensions#tabline#enabled'] = 0
-vim.g.airline_theme = 'molokai'
+vim.g['airline#extensions#branch#enabled'] = 1
+vim.g.airline_enabled = 1
+vim.g.airline_powerline_fonts = 1
+vim.g.airline_theme = 'badwolf'
+-- powerline symbols
+vim.g.airline_left_sep = ''
+vim.g.airline_left_alt_sep = ''
+vim.g.airline_right_sep = ''
+vim.g.airline_right_alt_sep = ''
+vim.g['airline_symbols.branch'] = ''
+vim.g['airline_symbols.readonly'] = ''
+vim.g['airline_symbols.linenr'] = ''
 
